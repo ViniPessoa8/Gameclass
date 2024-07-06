3b45f39b89d9042519fb707618e2954f95af5c99
Vinícius Pessoa <vini.pessoa7@gmail.com>
2024-07-06 13:17:17 -0400

    feat: Input component

---
+++ gameclass/src/lib/components/Input.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let type = "";
+    export let placeholder = ""; 
+</script>
+
+<input 
+    class="input"
+    type={type}
+    placeholder={placeholder}
+/>
+
+<style>
+    .input {
+        border:none;
+        font-family: var(--font);
+        border-bottom: 1px solid var(--cor-secundaria);
+        background-color: transparent;
+        color: var(--text-1);
+        font-size: 24px;
+    }
+
+    ::placeholder {
+        color: var(--cor-secundaria);
+    }
+</style>
