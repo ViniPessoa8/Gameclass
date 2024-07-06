2f48745f919bc7536a82c146c98b5aaeba6d251d
Vinícius Pessoa <vini.pessoa7@gmail.com>
2024-07-06 12:56:27 -0400

    feat: button

---
+++ gameclass/src/lib/components/Button.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<button class="button">
+    <slot></slot>
+</button>
+
+<style>
+    .button {
+        background-color: var(--cor-fundo);
+        border: none;
+        text-align: center;
+        height: 52px;
+        font-size: 28px;
+        font-family: var(--font);
+        font-weight: bold;
+        color: var(--cor-primaria);
+        border-radius: 15px;
+    }
+</style>
