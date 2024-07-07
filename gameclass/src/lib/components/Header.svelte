69969f3e3a95418bcba25b495f6bb1e3fed5abd6
Vinícius Pessoa <vini.pessoa7@gmail.com>
2024-07-07 16:10:54 -0400

    feat: Header

---
+++ gameclass/src/lib/components/Header.svelte
@@ -0,0 +1,45 @@
+<script>
+
+	import HeaderUserIcon from "./HeaderUserIcon.svelte";
+
+</script>
+<div class="header">
+    <h3>Gamiclass > Turmas</h3>
+    <div class="logged-user">
+        <div class="info">
+            <h4 class="logged-user-name">Eduardo Fonseca</h4>
+            <p class="logged-user-role">Professor</p>
+        </div>
+        <!-- icone do usuario -->
+        <HeaderUserIcon text={"E"} color={"#FF00FF"}/>
+    </div>
+</div>
+
+<style>
+    .header {
+        background-color: var(--cor-primaria);
+        display: flex;
+        color: white;
+        justify-content: space-between;
+        align-items: center;
+        padding:10px;
+        border-bottom-right-radius: 20px;
+        border-bottom-left-radius: 20px;
+    }
+
+    .logged-user {
+        display: flex;
+        gap: 10px;
+        text-align: end;
+    }
+
+    .logged-user-name {
+        font-size: 22px;
+    }
+
+    .logged-user-role {
+        font-size: 16px;
+        color: var(--cor-secundaria);
+        font-weight: bolder;
+    }
+</style>
---
+++ gameclass/src/lib/components/HeaderUserIcon.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let color, text = "";
+</script>
+
+<div class="icon" style="background-color: {color}">
+    <h1 class="info">
+        <slot>{text}</slot>
+    </h1>
+</div>
+
+<style>
+    .icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 60px;
+        width: 60px;
+        border-radius: 100%;
+    }
+
+    .icon > .info {
+        text-align: center;
+        justify-self: center;
+    }
+</style>
