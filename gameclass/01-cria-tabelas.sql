da91672c32f3828c455d529c82d81ae7c783721b
Vinícius Pessoa <vini.pessoa7@gmail.com>
2024-07-24 07:01:58 -0400

    fix: column 'nome' -> 'name'

--- gameclass/01-cria-tabelas.sql
+++ gameclass/01-cria-tabelas.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE usuario (
     "id" SERIAL,
-		"nome" VARCHAR(100) NOT NULL,
+		"name" VARCHAR(100) NOT NULL,
     "login" VARCHAR(100) NOT NULL,
     "hash" VARCHAR(255) NOT NULL,
     "salt" VARCHAR(255) NOT NULL
