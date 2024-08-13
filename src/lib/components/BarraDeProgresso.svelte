c9d5c3dbd8a2467a800ef4944994bd9e5d42c958
Vinicius Pessoa <vini.pessoa7@gmail.com>
2024-08-13 19:47:10 -0400

    feat: CircularTextIcon size and textSize props

--- src/lib/components/BarraDeProgresso.svelte
+++ src/lib/components/BarraDeProgresso.svelte
@@ -1,62 +1,59 @@
 <script>
-	import { COLORS } from "../constants";
+	import { COLORS } from '../constants';
 
-    // input[0] > input[1] > input[2] > ....
-    let input = [1, 3, 5]
-    
-    checkInput(input);
+	// input[0] > input[1] > input[2] > ....
+	let input = [1, 3, 5];
 
-    let barra_width = 390
-    let input_step = barra_width / input.length;
+	checkInput(input);
 
-    // Colors
-    const colors = [
-        COLORS.cor_secundaria_1, 
-        COLORS.cor_secundaria_2, 
-        COLORS.cor_secundaria_3,
-    ]
+	let barra_width = 390;
+	let input_step = barra_width / input.length;
 
-    function checkInput(input) {
-        if (!Array.isArray(input)) {
-            throw new Error("Input must be an array.");
-        }
+	// Colors
+	const colors = [COLORS.secundaria_1, COLORS.secundaria_2, COLORS.secundaria_3];
 
-        for (let i = 0; i < input.length; i++) {
-            if (input[i] > input[i+1]) {
-                throw new Error("The array must be in ascending order. (input[0] > input[1] > ...)")
-            }
-        }
-    }
+	function checkInput(input) {
+		if (!Array.isArray(input)) {
+			throw new Error('Input must be an array.');
+		}
+
+		for (let i = 0; i < input.length; i++) {
+			if (input[i] > input[i + 1]) {
+				throw new Error('The array must be in ascending order. (input[0] > input[1] > ...)');
+			}
+		}
+	}
 </script>
 
 <div class="barra-container">
-    {#each input as num, index}
-        <div 
-            class="barra-container barra-progresso"
-            style:width="{(index + 1) * input_step}px"
-            style:background-color="{colors[index]}"
-            style:z-index={100 - index}
-        >
-            {num}
-        </div>
-    {/each}
+	{#each input as num, index}
+		<div
+			class="barra-container barra-progresso"
+			style:width="{(index + 1) * input_step}px"
+			style:background-color={colors[index]}
+			style:z-index={100 - index}
+		>
+			{num}
+		</div>
+	{/each}
 </div>
 
 <style>
-    .barra-container {
-        margin-top: 4px;
-        width: 390px;
-        height: 24px;
-        border-radius: 40px;
-    }
+	.barra-container {
+		margin-top: 4px;
+		width: 390px;
+		height: 24px;
+		border-radius: 40px;
+	}
 
-    .barra-progresso {
-        border-radius: 40px;
-        position: absolute;
-        display: flex;
-        flex-direction: row;
-        justify-content: end;
-        box-sizing: border-box;
-        padding-right: 8.65px;
-    }
+	.barra-progresso {
+		border-radius: 40px;
+		position: absolute;
+		display: flex;
+		flex-direction: row;
+		justify-content: end;
+		box-sizing: border-box;
+		padding-right: 8.65px;
+	}
 </style>
+
--- src/lib/components/CircularTextIcon.svelte
+++ src/lib/components/CircularTextIcon.svelte
@@ -1,26 +1,35 @@
 <script>
-    export let color = "white";
-    export let backgroundColor = getComputedStyle(document.body).getPropertyValue("--cor-primaria");
+	export let color = 'white';
+	export let size = 60;
+	export let fontSize = 30;
+	export let backgroundColor = getComputedStyle(document.body).getPropertyValue('--cor-primaria');
 </script>
 
-<div class="icon" style="background-color: {backgroundColor}; color: {color}">
-    <h1 class="info">
-        <slot></slot>
-    </h1>
+<div
+	class="icon"
+	style="
+		background-color: {backgroundColor}; 
+		color: {color};
+		height: {size}px;
+		width: {size}px;
+		"
+>
+	<h1 class="info" style="font-size: {fontSize}px;">
+		<slot></slot>
+	</h1>
 </div>
 
 <style>
-    .icon {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 60px;
-        width: 60px;
-        border-radius: 100%;
-    }
+	.icon {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 100%;
+	}
 
-    .icon > .info {
-        text-align: center;
-        justify-self: center;
-    }
+	.icon > .info {
+		text-align: center;
+		justify-self: center;
+	}
 </style>
+
