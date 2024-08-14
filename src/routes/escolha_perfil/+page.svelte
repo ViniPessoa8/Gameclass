d1f78aae41c639a8e6128bd06dd16eb31bef387b
Vinicius Pessoa <vini.pessoa7@gmail.com>
2024-08-14 05:45:29 -0400

    feat: button icons + design fixes

--- src/routes/escolha_perfil/+page.svelte
+++ src/routes/escolha_perfil/+page.svelte
@@ -7,15 +7,15 @@
 	<h1>Olá, ${nome_usuario}!</h1>
 	<span>escolha seu perfil</span>
 	<div class="button-container">
-		<button>
-			<div class="botao aluno">
-				<img class="icon" alt="Ícone do perfil de aluno" />
+		<button class="button-estudante">
+			<div>
+				<img class="icon" alt="Ícone do perfil de aluno" src="icone-aluno.svg" />
 				<h1>Aluno</h1>
 			</div>
 		</button>
-		<button>
-			<div class="botao professor">
-				<img class="icon" alt="Ícone do perfil de professor" />
+		<button class="button-professor">
+			<div>
+				<img class="icon" alt="Ícone do perfil de professor" src="icone-professor.svg" />
 				<h1>Professor</h1>
 			</div>
 		</button>
@@ -31,6 +31,14 @@
 		margin-top: 8em;
 	}
 
+	.content-container > h1 {
+		font-size: 48px;
+	}
+
+	.content-container > span {
+		font-size: 32px;
+	}
+
 	.button-container {
 		display: flex;
 		flex-direction: row;
@@ -40,9 +48,23 @@
 
 	button {
 		padding: 35px;
+		/* padding-left: 80px; */
+		/* padding-right: 80px; */
+		width: 260px;
 		border-radius: 35px;
-		color: var(--text-1);
+		color: var(--cor-secundaria);
 		background-color: var(--cor-primaria);
 		border: 1px solid var(--cor-secundaria);
+	}
+
+	button h1 {
+		margin-top: 36px;
+		font-weight: 500;
+		font-size: 36px;
+	}
+
+	.button-professor {
+		background-color: var(--cor-secundaria);
+		color: var(--cor-primaria);
 	}
 </style>
