3f788b0518318b246c7c6c35f9501412dfa7c723
Vinicius Pessoa <vini.pessoa7@gmail.com>
2024-08-29 21:00:41 -0400

    config: add tests for auth.js

---
+++ src/lib/server/controllers/auth.test.js
@@ -0,0 +1,31 @@
+import { test, expect, afterAll } from "vitest";
+import { loginUser, registerNewUser, removeUserByLogin } from "./auth";
+
+test("Cria usuário válido", async () => {
+	await registerNewUser('Nome Completo', 'login123', 'Senhavalida!1', 'UEA', '2024-08-21', 'bio', "email@uea.edu.br", "1811440260")
+})
+
+test.fails("Falha ao criar usuário duplicado", async () => {
+	await registerNewUser('Nome Completo', 'login123', 'Senhavalida!1', 'UEA', '2024-08-21', 'bio', "email@uea.edu.br", "1811440260")
+})
+
+test.fails("Falha ao logar com senha incorreto", async () => {
+	await loginUser("login123", "Senhavalida!2")
+})
+
+test.fails("Falha ao logar sem usuário", async () => {
+	await loginUser("", "Senhavalida!2")
+})
+
+test.fails("Falha ao logar sem senha", async () => {
+	await loginUser("login123", "")
+})
+
+test("Loga com usuário criado", async () => {
+	await loginUser("login123", "Senhavalida!1")
+})
+
+test("Remove usuário criado", async () => {
+	await removeUserByLogin("login123")
+})
+
