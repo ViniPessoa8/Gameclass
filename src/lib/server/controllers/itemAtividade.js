6eed7d568fc4ea6fd15496405c71365b4cdee0b1
Vinicius Pessoa <vini.pessoa7@gmail.com>
2024-09-14 10:49:21 -0400

    fix: rename variable to idAtividadePai

--- src/lib/server/controllers/itemAtividade.js
+++ src/lib/server/controllers/itemAtividade.js
@@ -1,15 +1,12 @@
 import { cadastraItemAtividadeBD } from "../repositories/itemAtividade";
 
-export async function cadastraItemAtividade(titulo, notaMax, dataEntregaInicial, dataEntregaFinal, tipoAtribuicaoNota, emGrupos, receberAposPrazo, nIntegrantesGrupo, nMaxGrupos, idAtividade) {
-	console.log(titulo, notaMax, dataEntregaInicial, dataEntregaFinal, tipoAtribuicaoNota, emGrupos, receberAposPrazo, nIntegrantesGrupo, nMaxGrupos, idAtividade)
-	console.log(Boolean(!titulo || !notaMax || !dataEntregaInicial || !dataEntregaFinal || !tipoAtribuicaoNota || idAtividade))
-	if (!titulo || !notaMax || !dataEntregaInicial || !dataEntregaFinal || !tipoAtribuicaoNota || idAtividade <= 0) {
-		console.log(!titulo || !notaMax || !dataEntregaInicial || !dataEntregaFinal || !tipoAtribuicaoNota || idAtividade)
+export async function cadastraItemAtividade(titulo, notaMax, dataEntregaInicial, dataEntregaFinal, tipoAtribuicaoNota, emGrupos, receberAposPrazo, nIntegrantesGrupo, nMaxGrupos, idAtividadePai) {
+	if (!titulo || !notaMax || !dataEntregaInicial || !dataEntregaFinal || !tipoAtribuicaoNota || idAtividadePai <= 0) {
 		throw ("Dados obrigatórios não foram preenchidos.")
 	}
 
 	try {
-		let res = await cadastraItemAtividadeBD(titulo, notaMax, dataEntregaInicial, dataEntregaFinal, tipoAtribuicaoNota, emGrupos, receberAposPrazo, nIntegrantesGrupo, nMaxGrupos, idAtividade);
+		let res = await cadastraItemAtividadeBD(titulo, notaMax, dataEntregaInicial, dataEntregaFinal, tipoAtribuicaoNota, emGrupos, receberAposPrazo, nIntegrantesGrupo, nMaxGrupos, idAtividadePai);
 
 		if (res.rowCount > 0) {
 			return res.rows
@@ -20,4 +17,12 @@ export async function cadastraItemAtividade(titulo, notaMax, dataEntregaInicial,
 		}
 		throw e;
 	}
+}
+
+export async function removeItemAtividadePorId(idItemAtividade) {
+
+}
+
+export async function removeItemAtividadePorTitulo(titulo, idAtividadePai) {
+
 }
