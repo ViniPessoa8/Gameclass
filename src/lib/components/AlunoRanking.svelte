8ff1b0004a6cc8f736870198522b8e959d61d7ef
Vinicius Pessoa <vini.pessoa7@gmail.com>
2025-01-28 18:31:02 -0400

    feat: ranking de alunos

---
+++ src/lib/components/AlunoRanking.svelte
@@ -0,0 +1,28 @@
+<script>
+	import CircularTextIcon from './CircularTextIcon.svelte';
+
+	export let nome = '',
+		pontos = 0,
+		posicao = 0,
+		cor = 'var(--cor-primaria)';
+</script>
+
+<div class="container">
+	<CircularTextIcon backgroundColor={cor}>{nome[0]}</CircularTextIcon>
+	<div style="display: flex; flex-direction: column;">
+		<p style="font-size: 24px;"><b>{nome} #{posicao}</b></p>
+		<p style="font-size: 20px;">{pontos} pts</p>
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: row;
+		background-color: white;
+		margin: 8px;
+		padding: 8px 0px 8px 8px;
+		border-radius: 50px;
+		gap: 12px;
+	}
+</style>
---
+++ src/lib/components/RankingAlunos.svelte
@@ -0,0 +1,80 @@
+<script>
+	import AlunoRanking from './AlunoRanking.svelte';
+
+	export const pontosPorAluno = [
+		{
+			nome: 'Vinícius Pessoa',
+			pontos: 120,
+			cor: 'red'
+		},
+		{
+			nome: 'João Gabriel',
+			pontos: 130,
+			cor: 'green'
+		},
+		{
+			nome: 'Lucas Bezerra',
+			pontos: 90,
+			cor: 'blue'
+		},
+		{
+			nome: 'Pedro Henrique',
+			pontos: 95,
+			cor: 'cyan'
+		}
+	].sort(comparaPontos);
+
+	function comparaPontos(a, b) {
+		if (a.pontos < b.pontos) {
+			return 1;
+		}
+		if (a.pontos > b.pontos) {
+			return -1;
+		}
+		return 0;
+	}
+</script>
+
+<div class="container">
+	<div class="ranking">
+		<p class="titulo"><b>Ranking de Alunos - {pontosPorAluno.length}</b></p>
+		<hr />
+		<div class="top-3">
+			{#each pontosPorAluno.slice(0, 3) as pontoAluno, index}
+				<AlunoRanking
+					nome={pontoAluno.nome}
+					pontos={pontoAluno.pontos}
+					posicao={index + 1}
+					cor={pontoAluno.cor}
+				/>
+			{/each}
+		</div>
+		<hr />
+		<div class="other-ranking">
+			{#each pontosPorAluno.slice(3) as pontoAluno, index}
+				<AlunoRanking
+					nome={pontoAluno.nome}
+					pontos={pontoAluno.pontos}
+					posicao={index + 4}
+					cor={pontoAluno.cor}
+				/>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<style>
+	.container {
+		margin-top: -24px;
+		float: right;
+		background-color: var(--cor-secundaria);
+		width: 325px;
+		height: 1000px;
+	}
+
+	.titulo {
+		font-size: 24px;
+		text-align: center;
+		padding: 24px 0px;
+	}
+</style>
