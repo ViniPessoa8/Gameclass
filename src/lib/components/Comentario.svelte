faec745a00aef65828940bd2eb75393ef69e8ad5
Vinicius Pessoa <vini.pessoa7@gmail.com>
2025-01-28 20:04:04 -0400

    feat: sessão de comentarios

---
+++ src/lib/components/Comentario.svelte
@@ -0,0 +1,41 @@
+<script>
+	import CircularIcon from '$lib/components/CircularIcon.svelte';
+
+	export let nome = '';
+	export let data = '';
+	export let texto = '';
+</script>
+
+<div class="container">
+	<CircularIcon text={nome[0]} type="text" />
+	<div class="content">
+		<div class="titulo-data">
+			<p>{nome}</p>
+			<p class="data">{data}</p>
+		</div>
+		<p>{texto}</p>
+	</div>
+</div>
+
+<style scoped>
+	.container,
+	.titulo-data {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		padding-left: 12px;
+		gap: 8px;
+	}
+
+	.titulo-data {
+		justify-content: space-between;
+	}
+
+	.data {
+		text-align: right;
+	}
+</style>
--- src/routes/autenticado/turmas/[id]/atividades/[idAtividade]/[idEtapa]/+page.svelte
+++ src/routes/autenticado/turmas/[id]/atividades/[idAtividade]/[idEtapa]/+page.svelte
@@ -1,10 +1,26 @@
 <script>
 	import CircularIcon from '$lib/components/CircularIcon.svelte';
+	import Comentario from '$lib/components/Comentario.svelte';
 	import { page } from '$app/stores';
-	import Page from '../../+page.svelte';
 
 	const statusColor = 'blue';
 	const iconColor = 'red';
+	const descricaoEtapa =
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet lacinia felis. Quisque maximus sit amet magna quis dapibus. Quisque mollis dui vel nisi commodo, nec aliquet ante tempor. Suspendisse at eros tristique, volutpat mi faucibus, viverra nibh. Nullam sagittis, sem in viverra blandit, nulla felis sollicitudin arcu, eu maximus ligula justo non tortor. Mauris sollicitudin scelerisque sapien tempor maximus. Sed in cursus magna. Suspendisse potenti. Nulla dolor nisl, tristique sit amet bibendum nec, auctor nec risus. Aenean tincidunt mi purus, at mollis quam faucibus in. Sed dictum erat arcu, vitae feugiat justo gravida ut.';
+	const comentarios = [
+		{
+			nome: 'Vinícius Pessoa',
+			texto:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet lacinia felis. Quisque maximus sit amet magna quis dapibus.',
+			data: '06/12/1999'
+		},
+		{
+			nome: 'Lorena Simpson',
+			texto:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet lacinia felis. Quisque maximus sit amet magna quis dapibus.',
+			data: '06/12/1999'
+		}
+	];
 
 	let id, idAtividade, idEtapa;
 
@@ -13,39 +29,44 @@
 	$: idEtapa = $page.params.idEtapa;
 </script>
 
-<div class="content">
+<div class="content-etapa">
 	<div class="left-column">
 		<div class="page-info-container">
 			<div class="atividade-info-container">
 				<p class="titulo-atividade"><b>Nome da Atividade</b></p>
-				<p style="color: var(--cor-secundaria);">realização</p>
+				<p class="realizacao-atividade">realização</p>
 			</div>
 			<p><b>></b></p>
 			<!-- <div class="etapa-info-container"> -->
 			<!-- TODO: Fazer icone ter a mesma cor do icone da turma -->
 			<CircularIcon backgroundColor={iconColor} text="t" type="text" />
-			<p>Nome da Etapa</p>
-			<div style="display: flex; flex-direction: column; align-items: center;">
-				<p>Prazo: xx/xx/xxxx</p>
-				<p style="color: {statusColor}"><b>Status<b /></b></p>
+			<p class="titulo-etapa"><b>Nome da Etapa</b></p>
+			<div class="prazo-status-etapa">
+				<p class="prazo-etapa">Prazo: xx/xx/xxxx</p>
+				<p style="color: {statusColor}; font-size: 20px"><b>Status<b /></b></p>
 			</div>
 			<!-- </div> -->
 		</div>
 		<hr />
-		<div class="descricao-etapa"></div>
+		<div class="descricao-etapa">{descricaoEtapa}</div>
+		<hr />
 		<div class="comentarios-etapa">
-			<p># Comentários</p>
+			<p style="font-size:22px">{comentarios.length} Comentários</p>
 			<!-- TODO: Loop para mostrar componente Comentario.svelte -->
+			{#each comentarios as comentario}
+				<Comentario texto={comentario.texto} nome={comentario.nome} data={comentario.data} />
+			{/each}
 		</div>
 	</div>
 	<div class="right-column"></div>
 </div>
 
 <style scoped>
-	.content {
+	.content-etapa {
 		display: flex;
-		margin-top: 126px;
-		margin-left: 96px;
+		padding-top: 126px;
+		padding-left: 96px;
+		padding-right: 96px;
 	}
 
 	.left-column,
@@ -67,19 +88,19 @@
 		flex: 1;
 	}
 
-	.page-info-container,
-	.etapa-info-container {
+	.page-info-container {
 		display: flex;
 		flex-direction: row;
-	}
-
-	.etapa-info-container {
-		gap: 15px;
-		align-items: center;
+		padding-bottom: 12px;
 	}
 
 	.titulo-atividade {
-		font-size: 1.5rem;
+		font-size: 26px;
+	}
+
+	.realizacao-atividade {
+		font-size: 20px;
+		color: var(--cor-secundaria);
 	}
 
 	.page-info-container {
@@ -90,5 +111,29 @@
 
 	.descricao-etapa {
 		min-height: 348px;
+		font-size: 24px;
+		color: var(--cor-secundaria);
+	}
+
+	.titulo-etapa {
+		font-size: 24px;
+		color: var(--cor-primaria);
+	}
+
+	.prazo-status-etapa {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.prazo-etapa {
+		color: var(--cor-primaria);
+		font-size: 20px;
+	}
+
+	.comentarios-etapa {
+		display: flex;
+		flex-direction: column;
+		gap: 32px;
 	}
 </style>
