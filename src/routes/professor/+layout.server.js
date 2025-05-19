942a668cd84a14ccfff1e59bb640471b37da21a1
Vinícius Pessoa <vini.pessoa7@gmail.com>
2025-05-19 06:09:07 -0400

    fix: redirect /professor -> /professor/turmas

--- src/routes/professor/+layout.server.js
+++ src/routes/professor/+layout.server.js
@@ -2,12 +2,16 @@ import { redirect } from "@sveltejs/kit";
 import { getTurmasByIdProfessor } from "../../lib/server/controllers/turma";
 import { findUserByLogin } from "../../lib/server/repositories/auth";
 
-export async function load({ cookies }) {
+export async function load({ url, cookies }) {
 	const session_raw = cookies.get("session");
 	if (!session_raw) {
 		console.log("Usuário não autenticado")
 		cookies.set("toast", 'sessao_expirada', { path: '/' });
 		redirect(300, "/")
+	}
+
+	if (url.pathname == "/professor") {
+		redirect(301, "/professor/turmas")
 	}
 	const perfil_raw = cookies.get("perfil");
 	const data = JSON.parse(session_raw);
