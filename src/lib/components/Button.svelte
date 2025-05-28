d3b65e9895b91d12e11e4a57bada57e9b133f133
Vinícius Pessoa <vini.pessoa7@gmail.com>
2025-05-27 20:19:20 -0400

    feat: disabled on Button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -6,11 +6,13 @@
 	export let backgroundColor = '';
 	export let marginTop = '';
 	export let fontSize = '24px';
+	export let disabled = false;
 </script>
 
 <button
 	{type}
 	class="button"
+	{disabled}
 	style="background-color: {backgroundColor}; color: {color}; margin-top: {marginTop}; font-size: {fontSize}; width: {width}; height: {height}"
 	on:click
 	{...$$restProps}
@@ -21,7 +23,7 @@
 <style>
 	.button {
 		border: none;
-		height: 48px;
+		min-height: 48px;
 		font-family: var(--font);
 		font-weight: bold;
 		color: var(--cor-primaria);
@@ -29,7 +31,13 @@
 		padding: 10px 15px;
 	}
 
-	.button:hover {
+	.button:disabled {
+		color: darkgray;
+		background-color: lightgray;
+		cursor: default;
+	}
+
+	.button:enabled:hover {
 		background-color: #a1a1a1;
 		transition: 0.3s;
 		cursor: pointer;
