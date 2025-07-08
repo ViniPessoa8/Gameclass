922046783100f041eef825b13dab85b855670e58
Vinícius Pessoa <vini.pessoa7@gmail.com>
2025-07-08 19:17:05 -0400

    feat: página para escolher integrante do grupo a avaliar

---
+++ src/routes/professor/turmas/[id]/atividades/[idAtividade=integer]/[idEtapa]/[idEntrega]/escolhaIntegrante/+page.server.js
@@ -0,0 +1,70 @@
+import AtividadeController from "$lib/server/controllers/atividade";
+import ItemAtividadeController from "$lib/server/controllers/itemAtividade";
+import EntregaController from "$lib/server/controllers/entrega";
+import AvaliacaoController from '$lib/server/controllers/avaliacao';
+import GrupoController from '$lib/server/controllers/grupo';
+import ComentarioController from '$lib/server/controllers/comentario';
+import AnexoController from '$lib/server/controllers/anexo';
+import EstudanteController from '$lib/server/controllers/estudante';
+import UsuarioController from '$lib/server/controllers/usuario';
+import { goto, redirect } from "$app/navigation";
+
+const atividadeController = new AtividadeController()
+const itemAtividadeController = new ItemAtividadeController()
+const entregaController = new EntregaController()
+const avaliacaoController = new AvaliacaoController()
+const grupoController = new GrupoController()
+const comentarioController = new ComentarioController()
+const anexoController = new AnexoController()
+const estudanteController = new EstudanteController()
+const usuarioController = new UsuarioController()
+
+export async function load({ url, cookies, params }) {
+	const session_raw = cookies.get("session");
+	const data = JSON.parse(session_raw);
+	data["perfil"] = cookies.get("perfil")
+
+	const idEntrega = params.idEntrega
+	const idEtapa = params.idEtapa
+
+	const usuario = await usuarioController.buscaPorLogin(data.login)
+	const etapa = (await itemAtividadeController.buscaPorId(idEtapa)).toObject()
+	const atividade = (await atividadeController.buscaPorId(etapa.id_atividade)).toObject()
+	const entrega = (await entregaController.buscaPorId(idEntrega)).toObject()
+	const entregaAvaliada = await avaliacaoController.buscaPorIdEntrega(entrega.id)
+	const comentarios_entrega = await comentarioController.listaPorIdEntrega(parseInt(entrega.id))
+	const anexos = (await anexoController.listaPorIdEntrega(idEntrega)).map((e) => e.toObject())
+
+	let estudante, grupo;
+	if (etapa.em_grupos) {
+		grupo = await grupoController.buscaPorId(parseInt(entrega.id_grupo_de_alunos))
+		grupo.integrantes = await estudanteController.buscaPorIdGrupo(parseInt(entrega.id_grupo_de_alunos))
+		etapa.grupo = grupo
+	} else {
+		estudante = await estudanteController.buscaPorId(parseInt(entrega.id_estudante))
+	}
+
+	entrega["comentarios"] = comentarios_entrega
+	entrega["anexos"] = anexos
+	entrega["avaliada"] = entregaAvaliada ? true : false
+	usuario["cor"] = usuario.cor
+
+	let toast = ''
+	const message = cookies.get("toast");
+
+	if (message !== '') {
+		redirect(300, url.pathname + '/avaliacao')
+		toast = message
+	}
+
+	let returnData = {
+		"usuario": data,
+		"entrega": entrega,
+		"atividade": atividade,
+		"etapa": etapa,
+		"nome": etapa.em_grupos ? grupo.nome : estudante.nome,
+		"toast": toast
+	}
+
+	return returnData
+}
---
+++ src/routes/professor/turmas/[id]/atividades/[idAtividade=integer]/[idEtapa]/[idEntrega]/escolhaIntegrante/+page.svelte
@@ -0,0 +1,115 @@
+<script>
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import CardIntegrante from '$lib/components/CardIntegrante.svelte';
+	import { comentarios, fetchComentarios } from '$lib/../stores/listaComentarios.js';
+	import { STATUS_ENTREGA } from '$lib/constants.js';
+	import { Toaster, toast } from 'svelte-sonner';
+	import { goto } from '$app/navigation';
+
+	export let data;
+
+	let id;
+	let idAtividade;
+	let idEtapa;
+	let status, corStatus;
+	let listaComentarios;
+	let alunoParaAvaliar = null;
+
+	$: id = $page.params.id;
+	$: idAtividade = $page.params.idAtividade;
+	$: idEtapa = $page.params.idEtapa;
+	$: listaComentarios = comentarios;
+
+	async function calculaStatusEntrega() {
+		if (data.entrega.avaliada) {
+			status = STATUS_ENTREGA['2'];
+			corStatus = 'lightgreen';
+		} else if (data.entrega.anexos.length > 0) {
+			status = STATUS_ENTREGA['1'];
+		} else {
+			status = STATUS_ENTREGA['0'];
+		}
+	}
+
+	function onClickIntegrante(id) {
+		alunoParaAvaliar = id;
+		const url = $page.url.pathname.split('/').slice(0, -1).join('/') + '/' + id + '/avaliar';
+		goto(url);
+	}
+
+	onMount(async () => {
+		await fetchComentarios(data.entrega.id);
+		await calculaStatusEntrega();
+
+		if (data.toast === 'avaliacao_atualizada') {
+			toast.success('Avaliação editada com sucesso!');
+			data.toast = '';
+		}
+
+		if (data.toast === 'avaliacao_realizada') {
+			toast.success('Avaliação realizada com sucesso!');
+			data.toast = '';
+		}
+	});
+</script>
+
+<Toaster richColors position="top-center" closeButton />
+<div class="content-etapa">
+	<div class="page-info-container">
+		<div class="page-info-header">
+			<h1>{data.etapa.titulo}</h1>
+			<h3>{data.atividade.titulo}</h3>
+		</div>
+		<br />
+		<br />
+		<h2>Escolha um integrante para avaliar:</h2>
+		<div class="container-integrantes">
+			<div class="integrantes">
+				{#if data.etapa.grupo.integrantes && data.etapa.grupo.integrantes.length == 0}
+					<p>(Não há integrantes para avaliar)</p>
+				{:else}
+					{#each data.etapa.grupo.integrantes as integrante}
+						{console.debug('integrante => ', integrante)}
+						<CardIntegrante {integrante} on:click={() => onClickIntegrante(integrante?.id)} />
+					{/each}
+				{/if}
+			</div>
+		</div>
+	</div>
+</div>
+
+<style scoped>
+	.content-etapa {
+		width: 100%;
+		display: flex;
+		padding-top: 64px;
+	}
+
+	.page-info-header {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		padding-bottom: 8px;
+		justify-content: space-between;
+		align-items: center;
+		background-color: lightgray;
+	}
+
+	.page-info-container {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		padding-bottom: 8px;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.integrantes {
+		margin-top: 36px;
+		display: flex;
+		flex-direction: column;
+		align-items: left;
+	}
+</style>
