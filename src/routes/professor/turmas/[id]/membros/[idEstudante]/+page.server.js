f3f1bc4549a82f96a91c0c5030b4fcc9b2b1e0e4
Vinícius Pessoa <vini.pessoa7@gmail.com>
2025-07-22 21:13:46 -0400

    feat: boletim aluno: layout + dados backend

---
+++ src/routes/professor/turmas/[id]/membros/[idEstudante]/+page.server.js
@@ -0,0 +1,54 @@
+import TurmaController from "$lib/server/controllers/turma"
+import EstudanteController from "$lib/server/controllers/estudante"
+import AtividadeController from "$lib/server/controllers/atividade"
+import ItemAtividadeController from "$lib/server/controllers/itemAtividade"
+import AvaliacaoController from "$lib/server/controllers/avaliacao"
+import RankingController from "$lib/server/controllers/ranking"
+
+const turmaController = new TurmaController()
+const estudanteController = new EstudanteController()
+const atividadeController = new AtividadeController()
+const itemAtividadeController = new ItemAtividadeController()
+const avaliacaoController = new AvaliacaoController()
+const rankingController = new RankingController()
+
+
+export async function load({ params, cookies }) {
+	let data = {}
+	let idTurma = params.id
+	let idEstudante = params.idEstudante
+
+	data.turma = await turmaController.buscaPorCodigo(idTurma)
+	data.estudante = await estudanteController.buscaPorId(idEstudante)
+	let ranking = await rankingController.listaRankingPorIdTurma(idTurma)
+	ranking = ranking.sort((a, b) => a.pontos > b.pontos).map((e, i) => {
+		e.index = i + 1
+		return e
+	})
+
+	const estudante = ranking.filter((e) => e.id == idEstudante)
+	data.estudante.posicao_ranking = estudante[0].index
+	data.atividades = []
+	const atividades = await atividadeController.listaPorIdTurma(idTurma)
+
+
+	for (const a of atividades) {
+		a.itens_atividade = await itemAtividadeController.listaPorIdAtividade(a.id)
+		for (const ia of a.itens_atividade) {
+			ia.criterios = await itemAtividadeController.listaCriteriosPorId(a.id)
+			ia.notas = await itemAtividadeController.listaNotasDeCriteriosPorId(a.id)
+			ia.avaliacao = await avaliacaoController.buscaPorEstudanteItemAtividade(ia.id, idEstudante)
+		}
+
+		data.atividades.push(a);
+	}
+
+	data.atividades = atividades.map((a) => a.toObject())
+
+	// if (estudantes && estudantes.length == 0) {
+	// 	return { estudantes: [] }
+	// }
+	//
+	// data["estudantes"] = estudantes
+	return data
+}
---
+++ src/routes/professor/turmas/[id]/membros/[idEstudante]/+page.svelte
@@ -0,0 +1,135 @@
+<script>
+	import { Toaster, toast } from 'svelte-sonner';
+	import TurmaTabBar from '$lib/components/TurmaTabBar.svelte';
+	import selectedTurmaTabBar from '$src/stores/selectedTurmaTabBar.js';
+	import { onMount } from 'svelte';
+
+	export let data;
+
+	const estudante = data.estudante;
+	const turma = data.turma;
+	const atividades = data.atividades;
+
+	onMount(() => {
+		// for (const atividade of atividades) {
+		for (let i = 0; i < atividades.length; i++) {
+			if (atividades[i].itens_atividade.length == 0) {
+				atividades[i].media_obtida = '-';
+				atividades[i].media_max = '-';
+			} else {
+				atividades[i].media_obtida = (
+					atividades[i].itens_atividade.reduce((acc, ia) => {
+						if (!ia.avaliacao) return acc;
+
+						return (
+							acc +
+							ia.avaliacao?.criterios_avaliados.reduce((acc2, a) => a.nota_atribuida + acc2, 0) /
+								ia.avaliacao?.criterios_avaliados.length
+						);
+					}, 0) / atividades[i].itens_atividade.length
+				).toFixed(1);
+
+				atividades[i].media_max = (
+					atividades[i].itens_atividade.reduce(
+						(acc, ia) => acc + ia.criterios.reduce((acc2, c) => c.pontuacao_max + acc2, 0),
+						0
+					) / atividades[i].itens_atividade.length
+				).toFixed(1);
+			}
+		}
+	});
+</script>
+
+<Toaster richColors position="top-center" closeButton />
+<TurmaTabBar />
+<div class="content-membro">
+	<h1>Boletim do Aluno</h1>
+	<div class="estudante-info">
+		<div class="row">
+			<p><b>{estudante.login}</b></p>
+			<p>{estudante.nome}</p>
+		</div>
+		<p>{turma.nome} - {turma.disciplina}</p>
+		<p>Posição no ranking da turma: <b>{estudante.posicao_ranking}</b></p>
+	</div>
+	<div class="atividades">
+		<h1>Atividades</h1>
+		<hr />
+		<div class="atividades-container">
+			{#each atividades as atividade}
+				<div class="atividade">
+					<h2>{atividade.titulo}</h2>
+					<p style="font-size: 18px">Média</p>
+					<h3>{atividade.media_obtida}/{atividade.media_max}</h3>
+					<hr style="border-color: var(--cor-secundaria)" />
+					<!-- for item atividade -->
+					{#each atividade.itens_atividade as itemAtividade, index}
+						<p style="font-size:18px"><u><b>{index + 1}. {itemAtividade.titulo}</b></u></p>
+						<p style="font-size:18px">
+							{#if itemAtividade.avaliacao?.criterios_avaliados.filter((a) => a.nota_atribuida)}
+								{itemAtividade.avaliacao?.criterios_avaliados.reduce(
+									(acc, a) => a.nota_atribuida + acc,
+									0
+								) / itemAtividade.avaliacao?.criterios_avaliados.length}
+								/ {itemAtividade.criterios.reduce((acc, c) => c.pontuacao_max + acc, 0)}
+							{:else}
+								- / {itemAtividade.criterios.reduce((acc, c) => c.pontuacao_max + acc, 0)}
+							{/if}
+						</p>
+					{/each}
+					{#if atividade.itens_atividade.length == 0}
+						<p style="font-size:18px">(Não há itens cadastrados)</p>
+					{/if}
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
+
+<style scoped>
+	.content-membro {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 24px;
+		gap: 24px;
+		padding: 24px 128px;
+	}
+
+	.row {
+		display: flex;
+		flex-direction: row;
+		gap: 12px;
+	}
+
+	.estudante-info {
+		font-size: 20px;
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		text-align: left;
+	}
+
+	.atividades-container {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.atividade {
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: column;
+		background-color: var(--cor-secundaria-1);
+		border: 1px solid black;
+		border-radius: 38px;
+		padding: 18px;
+		margin: 12px;
+		text-align: center;
+		gap: 12px;
+	}
+
+	hr {
+		border: none;
+		border-top: 1px solid var(--cor-secundaria);
+	}
+</style>
